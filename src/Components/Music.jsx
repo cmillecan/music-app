14ef079b5a0a9028302b6299d902fefe0eda61df
cmillecan <cmillecan@gmail.com>
2021-05-19 23:37:37 -0700

    add useState and useEffect for notes played

--- src/Components/Music.jsx
+++ src/Components/Music.jsx
@@ -1,34 +1,52 @@
-import React from 'react'
-import './Music.css'
-import * as Tone from 'tone'
-import notes from "./notes"
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Music.css";
+import * as Tone from "tone";
+import notes from "./notes";
 
-const octaves = [4, 5]
+const octaves = [3, 4];
 
 export default function Music() {
+  const [notesPlayed, setNotesPlayed] = useState([]);
+  useEffect(() => {
+    if (notesPlayed.length === 3) {
+      console.log(notesPlayed);
+      setNotesPlayed([]);
+    }
+  }, [notesPlayed]);
   const synth = new Tone.Synth().toDestination();
   const playNote = (note, octave) => {
-    synth.triggerAttackRelease(`${note}${octave}`, '8n')
-  }
+    synth.triggerAttackRelease(`${note}${octave}`, "8n");
+  };
 
   const renderNotes = () => {
-    let noteButtons = []
-    octaves.forEach(o => {
-      notes.forEach(n => {
-        const absNote = `${n.name}`
-        noteButtons.push(<button key={absNote} className={n.isWhite ? "white-key" : "black-key"} onClick={() => playNote(n.name, o)}>{absNote}</button>)
-      })
-    })
+    let noteButtons = [];
+    octaves.forEach((o) => {
+      notes.forEach((n) => {
+        noteButtons.push(
+          <button
+            key={`${n.name}${o}`}
+            className={n.isWhite ? "white-key" : "black-key"}
+            onClick={() => {
+              playNote(n.name, o);
 
-    return noteButtons
-  }
+              setNotesPlayed([...notesPlayed, n.name]);
+            }}
+          >
+            {n.name}
+          </button>
+        );
+      });
+    });
+
+    return noteButtons;
+  };
 
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <h1>Musical Keyboard Game</h1>
-      <div className='keyboard'>
-        {renderNotes()}
-      </div>
+      <div className="keyboard">{renderNotes()}</div>
+      <div>Notes Played: {notesPlayed}</div>
     </div>
-  )
+  );
 }
