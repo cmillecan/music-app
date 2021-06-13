ec9ec8956bec28b89864c94f30872a11e83f8e36
cmillecan <cmillecan@gmail.com>
2021-06-13 00:52:53 -0700

    remove router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
 import Music from "./Components/Music.jsx";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/game">
-            <Music />
-          </Route>
-          <Redirect from="/" to="/game" exact />
-          <Route path="/">404</Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+  return <Music />;
 }
 
 export default App;
